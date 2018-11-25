7145af67fd188188d6f7231d7125750717fe40e0
Romulus Crisan <cromica@gmail.com>
2018-11-25 11:48:54 +0200

    Add project files.

---
+++ Trados.Studio.QuickInfo/QuickInfoAction.cs
@@ -0,0 +1,37 @@
+﻿using Sdl.TellMe.ProviderApi;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TradosStudioQuickInfo
+{
+    public class QuickInfoAction : ITellMeAction
+    {
+        private readonly object resultNode;
+
+        public QuickInfoAction(string processorName, string text)
+        {
+            this.Description = $"Results from {processorName}";
+            this.Name = text;
+        }
+
+        public string Name { get; set; } = "QuickInfo link";
+
+        public string Category => "QuickInfo results";
+
+        public string Description { get; set; } = "Opens a link returned in a quick info search";
+
+        public Icon Icon => PluginResources.info_1930258;
+
+        public bool IsAvailable => true;
+
+        public void Execute()
+        {
+            Clipboard.SetText(Name);
+        }
+    }
+}
---
+++ Trados.Studio.QuickInfo/QuickInfoProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TellMe.ProviderApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Extensions;
+
+namespace TradosStudioQuickInfo
+{
+    [TellMeSearchProvider]
+    public class QuickInfoProvider : ITellMePluginLoader
+    {
+        public ISearchDataProvider InitializeProvider()
+        {
+            var quickInfoSearchDataProvider = new QuickInfoSearchDataProvider();
+            return quickInfoSearchDataProvider;
+        }
+    }
+}
